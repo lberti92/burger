2790be644d80311916d520397b161b26d4cf2591
Breeze <lberti@msn.com>
2019-07-25 17:55:18 -0600

    able to add new burger

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Meeses');
+INSERT INTO burgers (burger_name) VALUES ('Bobbi');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester', true);
+INSERT INTO burgers (burger_name) VALUES ('Marilyn Manson');
+INSERT INTO burgers (burger_name) VALUES ('Joe Biden');
+INSERT INTO burgers (burger_name) VALUES ('Owens');
