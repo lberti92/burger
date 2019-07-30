34762579ea4391c1b7f3b73aae89fa64048d8088
Breeze <lberti@msn.com>
2019-07-29 18:54:01 -0600

    updated HTML and added delete button for CRUD

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,3 @@
-// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 var burger = {
@@ -18,12 +17,12 @@ var burger = {
       cb(res);
     });
   },
-  // delete: function(condition, cb) {
-  //   orm.delete("cats", condition, function(res) {
-  //     cb(res);
-  //   });
-  // }
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
 };
 
-// Export the database functions for the controller (catsController.js).
+
 module.exports = burger;
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,58 +3,44 @@ $(function() {
   $(".change-burger").on("click", function(event) {
     var id = $(this).data("id");
     console.log("id" + id)
- 
-
     var newburgerState = {
       devoured: true
     };
 
-    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newburgerState
     }).then(
       function() {
-        // $("change-sleep").disabled();
         location.reload();
       }
     );
   });
 
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
+  $(".burger-input").on("submit", function(event) {
     event.preventDefault();
-
     var newBurger = {
       name: $("#ca").val().trim(),
-  
     };
 
-    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
-        console.log("added a new burger");
-
         location.reload();
       }
     );
   });
 
-  // $(".delete-cat").on("click", function(event) {
-  //   var id = $(this).data("id");
-
-  //   // Send the DELETE request.
-  //   $.ajax("/api/cats/" + id, {
-  //     type: "DELETE"
-  //   }).then(
-  //     function() {
-  //       console.log("deleted cat", id);
-  //       // Reload the page to get the updated list
-  //       location.reload();
-  //     }
-  //   );
-//   });
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        location.reload();
+      }
+    );
+  });
 });
